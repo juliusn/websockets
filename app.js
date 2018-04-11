ab652c4c3bf6f14b78ea95ecfa8376e1bac89b41
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2018-04-11 14:38:14 +0300

    initial

---
+++ app.js
@@ -0,0 +1,61 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
+const port = 3100;
+const Server = require('http').Server;
+const http = new Server(app);
+const io = require('socket.io')(http);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+io.on('connection', (socket) => {
+  const id = socket.id;
+  socket.on('message', (msg) => {
+    console.log(id, msg);
+    io.emit('message', msg);
+  });
+
+  console.log('connected: ' + id);
+
+  socket.on('disconnect', () => {
+    console.log('disconnected: ' + id);
+  });
+});
+
+http.listen(port, () => {
+  console.log('listening on *:' + port);
+});
+
+module.exports = app;
